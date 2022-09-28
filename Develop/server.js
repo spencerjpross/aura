60caed5d43d7e9f73106261372cf3f162d606074
nofutofu <aaronrweis@gmail.com>
2022-09-27 19:07:41 -0500

    first draft of server & auth util

---
+++ Develop/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SeqStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// potential to add helpers for handlebars here
+const hbs = exphbs.create({});
+
+// constructing session/cookie
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SeqStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Listening for server at ${PORT}`));
+});
---
+++ Develop/utils/auth
@@ -0,0 +1,9 @@
+const isAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = isAuth;
