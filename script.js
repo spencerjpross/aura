bab6ae83b567ac1d80c8a5d029c97d8e28f3f0f4
angeltried <angeltried@icloud.com>
2022-09-28 08:57:19 -0500

    first feature/html push

---
+++ script.js
@@ -0,0 +1,32 @@
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+
+  function startTime(){
+  var todayDate = new Date();
+ 
+        //Gets the Date Values
+        var dd = ("0" + todayDate.getDate()).slice(-2);
+        var mm = ("0" + (todayDate.getMonth() + 1)).slice(-2);
+        var yyyy = todayDate.getFullYear();
+        document.getElementById('currentDate').innerHTML = mm + " / " + dd + " / " + yyyy;
+    
+ 
+        //Gets the Time Values
+        var hh = ("0" + todayDate.getHours()).slice(-2);
+        var min = ("0" + todayDate.getMinutes()).slice(-2);
+        var ss = ("0" + todayDate.getSeconds()).slice(-2);
+        document.getElementById('currentTime').innerHTML = hh + " : " + min + " : " + ss;
+        t = setTimeout(function() {
+            startTime()
+          }, 500);
+ 
+        //Sets the Current Data
+        var currentMonth = ("0" + (todayDate.getMonth() + 1)).slice(-2);
+        var currentHour = (("0" + todayDate.getHours()).slice(-2)) * 100;
+  }
+  startTime();
