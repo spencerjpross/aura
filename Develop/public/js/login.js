3234161df9036d37350bab10dbf7bcb56e0f98ac
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-09-27 19:53:58 -0500

    edits
    making some edits to the handlebars and javascript

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -1,7 +1,7 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
