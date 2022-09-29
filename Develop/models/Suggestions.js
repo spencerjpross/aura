16367accf3f53b1c5267b53acaf6fc82bed8281d
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2022-09-29 18:44:42 -0500

    created suggestions model

---
+++ Develop/models/Suggestions.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+ class Suggestions extends Model {}
+
+ Suggestions.init(
+     {
+         id: {
+             type: DataTypes.INTERGER,
+             allowNull: false,
+             primaryKey: true,
+             autoIncrement: true,
+         },
+         title: {
+             type: DataTypes.STRING,
+             allowNull: false,
+         },
+         mood_id: {
+             type: DataTypes.INTERGER,
+             refrences: {
+                 model: 'mood',
+                 key: 'id'
+             },
+         },
+         sequelize,
+         timestamps: false,
+         freeTableNames: true,
+         underscored: true,
+         modelName: 'suggestions',
+     }
+ );
+
+ module.exports = Suggestions;
