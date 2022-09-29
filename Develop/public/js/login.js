827c590f607df1c5055d179e252120cbcf8b08b1
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-09-29 17:37:56 -0500

    more css, adding id's

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
     if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
