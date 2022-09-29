369c5668c5825538875c5605f58e477f4b323f4e
angeltried <angeltried@icloud.com>
2022-09-28 20:30:19 -0500

    wed night update

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function checkTime(i) {
         var dd = ("0" + todayDate.getDate()).slice(-2);
         var mm = ("0" + (todayDate.getMonth() + 1)).slice(-2);
         var yyyy = todayDate.getFullYear();
-        document.getElementById('currentDate').innerHTML = mm + " / " + dd + " / " + yyyy;
+        document.getElementById('currentDate').innerHTML = mm + "/" + dd + "/" + yyyy;
     
  
         //Gets the Time Values
         var hh = ("0" + todayDate.getHours()).slice(-2);
         var min = ("0" + todayDate.getMinutes()).slice(-2);
         var ss = ("0" + todayDate.getSeconds()).slice(-2);
-        document.getElementById('currentTime').innerHTML = hh + " : " + min + " : " + ss;
+        document.getElementById('currentTime').innerHTML = hh + ":" + min + ":" + ss;
         t = setTimeout(function() {
             startTime()
           }, 500);
