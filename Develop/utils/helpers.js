077b687d1109250dcaa56a91a8c6040c602a5854
nofutofu <aaronrweis@gmail.com>
2022-10-04 06:48:32 -0500

    added comments

--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -1,10 +1,12 @@
 module.exports = {
 
+    // generates a pie chart with recieved mood data
     gen_chart: (data) => {
         let ag = f = s = b = ax = h = e = 0;
 
         for (let i = 0; i < data.length; i++) {
 
+            // checks which mood the piece of data has
             switch (data[i].mood_name) {
                 case 'Angry':
                     ag++;
@@ -30,6 +32,7 @@ module.exports = {
             }
         }
 
+        // generates using billboard.js
         var chart = bb.generate({
             bindto: "#chart",
             title: {
@@ -63,9 +66,11 @@ module.exports = {
         });
     },
 
+    // generates a calendar with the users monthly mood data
     gen_calendar: (data) => {
         const calendar = document.getElementById('calendar');
         const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        // data for testing because this is a work in progress
         data = [{
             date: "10/8/2022",
             mood_id: "1",
@@ -86,6 +91,7 @@ module.exports = {
             const month = dt.getMonth();
             const year = dt.getFullYear();
 
+            // finds the first day of the month as well as how many days in the month
             const firstDay = new Date(year, month, 1);
             const daysPerMonth = new Date(year, month + 1, 0).getDate();
 
@@ -97,6 +103,7 @@ module.exports = {
          });
             const dayPadding = weekdays.indexOf(dateString.split(', ')[0]);
 
+            // creates the classes for each individual box per day 
         for(let i = 1; i <= dayPadding + daysPerMonth; i++) {
             const daySquare = document.createElement('div');
             daySquare.classList.add('day');
@@ -106,6 +113,8 @@ module.exports = {
                 
             for (let v = 0; v <= data.length; v++) {
                 
+                // work in progress to check each day through the loop and assign a mood related
+                // background color to them
                 if (data[v].date == (month + 1) + "/" + (i - dayPadding) + "/" + year) {
                 switch (data.mood_id) {
                     case "1": 
@@ -135,12 +144,14 @@ module.exports = {
               }
             }
         
+            // numbers each boxes taking in to count the previously determined "padding days"
+            // to start the month on the correct day
             if (i > dayPadding) {
             daySquare.innerText = i - dayPadding;
 
             }
+            // constructs each square appending them on to each other
             calendar.appendChild(daySquare);
         }
-        console.log((month + 1) + "/" + day + "/" + year);
     },
 };
