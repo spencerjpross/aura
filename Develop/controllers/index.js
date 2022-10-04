9fe0f6859ec53051758e1d9f60529d0ffd115e56
nofutofu <aaronrweis@gmail.com>
2022-10-04 18:45:43 -0500

    prepping to merge dev

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
 const homeRoutes = require('./homepageRoute');
 const apiRoutes = require('./api')
-const app = express();
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes)
 
 
 // Default route for any other url's
-app.get("*", (req, res) => {
-    res.send("PAGE NOT FOUND");
-  });
+// app.get("*", (req, res) => {
+//     res.send("PAGE NOT FOUND");
+//   });
 
 
   module.exports = router;
